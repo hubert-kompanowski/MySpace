f1c74f037a762400279ee919bdcbf413acdcfb30
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2017-11-28 22:02:10 +0100

    stopnie i radiany

--- JiMP/Zajecia3/zad3.c
+++ JiMP/Zajecia3/zad3.c
@@ -2,12 +2,47 @@
 
 #include <stdio.h>
 
+void stopnie_na_radiany();
+void radiany_na_stopnie();
+
 int main(void)
 {
-    double stopnie, radiany, radiany_z_pi;
+    int zmienna;
+    printf ("\nJaki przelicznik wybierasz?\n");
+    printf ("1. Z stopni na radiany\n");
+    printf ("2. Z radianow na stopnie\n");
+    scanf("%d",&zmienna);
+    
+    switch (zmienna)
+    {
+        case 1:
+            stopnie_na_radiany();
+            break;
+        case 2:
+            radiany_na_stopnie();
+            break;
+        default:
+            printf("Nie ma takiego numeru");
+            break;
+    }
+}
+
+void stopnie_na_radiany()
+{
+    double stopnie, radiany;
+    double pi = 3.14159;
     printf("Podaj liczbe stopni: ");
     scanf("%lf", &stopnie);
-    radiany = stopnie / 360 * 2 * 3.14159;
-    radiany_z_pi = stopnie / 360 * 2;
-    printf("%g stopni to %g (%g*pi) radianow\n", stopnie, radiany, radiany_z_pi);
+    radiany = stopnie / 360 * 2 * pi;
+    printf("%lf stopni to %lf radianow.\n", stopnie, radiany);
+}
+
+void radiany_na_stopnie()
+{
+    double stopnie, radiany;
+    double pi = 3.14159;
+    printf("Podaj liczbe radianow: ");
+    scanf("%lf", &radiany);
+    stopnie = radiany * 360 /( 2 * pi);
+    printf("%lf radianow to %lf stopnie.\n", radiany, stopnie);
 }
